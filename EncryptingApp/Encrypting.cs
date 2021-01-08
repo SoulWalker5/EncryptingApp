dcb5a2985784b8bf391968f590f245f499844319
Soulwalker5 <shrituon@gmail.com>
2021-01-08 23:26:58 +0200

    Add project files.

---
+++ EncryptingApp/Encrypting.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EncryptingApp
+{
+    public static class Encrypting
+    {
+        /// <summary>
+        /// Return encrypted string
+        /// </summary>
+        /// <param name="text">Input string</param>
+        /// <param name="n">Repeats of encrypting</param>
+        /// <returns></returns>
+        public static string Encrypt(string text, int n)
+        {
+            if (string.IsNullOrEmpty(text) || n <= 0)
+            {
+                return text;
+            }
+
+            text = text.ToLower();
+
+            while (n != 0)
+            {
+                string outputTextPart2 = default;
+                string outputTextPart1 = default;
+                string outputText = default;
+
+                n--;
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        outputTextPart2 += text[i];
+                    }
+                    else
+                    {
+                        outputTextPart1 += text[i];
+                    }
+                    outputText = outputTextPart1 + outputTextPart2;
+                }
+
+                text = outputText;
+            }
+
+            return text;
+        }
+
+        /// <summary>
+        /// Return decrypted string
+        /// </summary>
+        /// <param name="encryptedText">Input string</param>
+        /// <param name="n">Repeats of decrypting</param>
+        /// <returns></returns>
+        public static string Decrypting(string encryptedText, int n)
+        {
+            if (string.IsNullOrEmpty(encryptedText) || n <= 0)
+            {
+                return encryptedText;
+            }
+
+            encryptedText = encryptedText.ToLower();
+
+            while (n != 0)
+            {
+                string outputText = default;
+                int middle = (encryptedText.Length / 2);
+
+                n--;
+
+                for (int i = 0; i < middle; i++)
+                {
+                    outputText += encryptedText[i + middle];
+                    outputText += encryptedText[i];
+
+                    if (encryptedText.Length % 2 != 0 && i == middle - 1)
+                    {
+                        outputText += encryptedText[encryptedText.Length - 1];
+                    }
+                }
+
+                encryptedText = outputText;
+            }
+
+            return encryptedText;
+        }
+
+        /// <summary>
+        /// Return 3 most common word
+        /// </summary>
+        /// <param name="text">Input string</param>
+        /// <returns></returns>
+        public static string[] MostCommonWords(string text)
+        {
+            text = text.ToLower();
+
+            char[] separators = { '\\', '/', ',', '.', '?', '!', ';', '{', '}', '[', ']', ':', '*', '@', '#', '$', '№', '&', '+', '<', '>', '|', '~', '`', '"', '_', '^', '(', ')', ' ' };
+
+            var newText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = newText.Count -1 ; i >= 0; i--)
+            {
+                if (newText[i].Equals("-"))
+                {
+                    newText.RemoveAt(i); ;
+                }
+            }
+
+            int[] counter = new int[newText.Count];
+            List<(string word, int number)> textList = new List<(string myString, int myInt)>();
+
+            for (int i = newText.Count - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (newText[i].Equals(newText[j]))
+                    {
+                        counter[i] += 1;
+                    }
+                }
+                textList.Add((newText[i], counter[i]));
+            }
+            var topThreeWords = textList.Distinct().OrderByDescending(x => x.number).Take(3).Where(x => x.number > 0);
+            var sortedArray = topThreeWords.Select(x => x.word).ToArray();
+
+            if (sortedArray.Length < 3)
+            {
+                Array.Clear(sortedArray, 0, sortedArray.Length);
+                return sortedArray;
+            }
+
+            return sortedArray;
+        }
+    }
+}
---
+++ EncryptingApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EncryptingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encrypted string is: " + Encrypting.Encrypt("", 1));
+            Console.WriteLine("Decrypted string is: " + Encrypting.Decrypting("", 1));
+            Console.WriteLine("Encrypted string is: " + Encrypting.Encrypt(null, 1));
+            Console.WriteLine("Decrypted string is: " + Encrypting.Decrypting(null, 1));
+            Console.WriteLine("Encrypted string is: " + Encrypting.Encrypt("Abcdefghij", 1));
+            Console.WriteLine("Decrypted string is: " + Encrypting.Decrypting("bdfhacegi", 1));
+            Console.WriteLine("Encrypted string is: " + Encrypting.Encrypt("Abcdefghi", 1));
+            Console.WriteLine("Decrypted string is: " + Encrypting.Decrypting("bdfhjacegi", 1));
+            Console.WriteLine("Encrypted string is: " + Encrypting.Encrypt("Abcdefghij", 2));
+            Console.WriteLine("Decrypted string is: " + Encrypting.Decrypting("dhcgbfaei", 2));
+
+            var hz = "ajhsgfuyatgiuyf two-way two-way - -";
+            foreach (var item in Encrypting.MostCommonWords(hz))
+            {
+                Console.WriteLine(item);
+            }
+
+            var hz1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+                "It has survived not only five centuries, but also the leap into electronic typesetting," +
+                " remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset" +
+                " sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like" +
+                " Aldus PageMaker including versions of Lorem Ipsum.";
+
+            foreach (var item in Encrypting.MostCommonWords(hz1))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
